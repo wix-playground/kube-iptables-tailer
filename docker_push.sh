580177dc6c5602a8f6023e69b9674358757d4e43
alexshe <alexshe@wix.com>
2019-06-23 16:50:51 +0300

    Change docker name

--- docker_push.sh
+++ docker_push.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-docker push alexshe/kube-iptables-tailer:$TRAVIS_BUILD_NUMBER
+docker push alexshemesh/kube-iptables-tailer:$TRAVIS_BUILD_NUMBER
