33a47ea3524897e44ad67ffab5ea0ac9359dce17
alexshe <alexshe@wix.com>
2019-06-23 17:04:07 +0300

    Refactoring

--- docker_push.sh
+++ docker_push.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
+
+docker build -t alexshemesh/kube-iptables-tailer .
+docker tag alexshemesh/kube-iptables-tailer alexshemesh/kube-iptables-tailer:$TRAVIS_BUILD_NUMBER
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 docker push alexshemesh/kube-iptables-tailer:$TRAVIS_BUILD_NUMBER
